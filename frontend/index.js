87e4d265a1c4b4928d370ca55787dceb6e3cbc08
Matt <64214044+kvbc@users.noreply.github.com>
2022-06-05 15:42:18 +0200

    Add files via upload

---
+++ frontend/index.js
@@ -0,0 +1,58 @@
+const APIUrl = "http://localhost:8080/api/user";
+const APIRegisterUrl = "http://localhost:8080/api/user/register";
+const APILoginUrl = "http://localhost:8080/api/user/login";
+
+function jsonEmpty (json) {
+    return Object.keys(json).length === 0;
+}
+
+function APIRequest (method, url, json, callback) {
+    var req = {
+        "url": url,
+        "method": method,
+        "success": callback
+    };
+
+    // empty JSON
+    if (Object.keys(json).length == 0) {
+        return $.ajax(req);
+    }
+
+    req.dataType = "json";
+    req.contentType = "application/json";
+    req.data = JSON.stringify(json);
+
+    return $.ajax(req);
+}
+
+function updateRegisteredUsersTable () {
+    $("#users").html('<tr><th>ID</th><th>Login</th><th>Password Hash</th></tr>')
+    APIRequest("GET", APIUrl, {}, (d) => {
+        d.forEach(u => {
+            $("#users").append(`<tr><td>${u.id}</td><td>${u.login}</td><td>${u.password}</td></tr>`)
+        });
+    });
+}
+
+function APILoginRegisterDelete (method, url, success_msg) {
+    APIRequest(method, url, {
+        "login": $("#i-login").val(),
+        "password": $("#i-pwd").val()
+    }, (d) => {
+        if (d.user == null) {
+            $("#out").css("color", "red");
+            $("#out").html(d.msg);
+        } else {
+            $("#out").css("color", "green");
+            $("#out").html(success_msg);
+            updateRegisteredUsersTable();
+        }
+    })
+}
+
+$(document).ready(() => {
+    updateRegisteredUsersTable();
+    $("#btn-log").on("click", () => APILoginRegisterDelete("POST", APILoginUrl, "Logged in successfully"));
+    $("#btn-reg").on("click", () => APILoginRegisterDelete("POST", APIRegisterUrl, "Your account has been registered"));
+    $("#btn-del").on("click", () => APILoginRegisterDelete("DELETE", APIUrl, "Your account has been deleted"));
+})
